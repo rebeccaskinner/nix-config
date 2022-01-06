0a9bb0ac8aa3b952b30524ac0989f672c4dfa504
rebecca skinner <rebecca@rebeccaskinner.net>
2022-01-05 20:48:46 -0600

    cleanup

--- home.nix
+++ home.nix
@@ -2,8 +2,6 @@
 
 let
   load     = f: import f { inherit pkgs utils; };
-  defaults = import ./collections/defaults/system-defaults.nix;
-  configs  = import ./configs/defaults/system-defaults.nix;
   utils    = import ./utils;
   games    = load ./collections/games;
 
