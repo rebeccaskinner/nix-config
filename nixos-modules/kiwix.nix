eb25c2928aa53ab044b01815bcc42c060fb17d28
rebecca skinner <rebecca@rebeccaskinner.net>
2023-05-03 19:57:51 -0500

    kiwix server update

--- nixos-modules/kiwix.nix
+++ nixos-modules/kiwix.nix
@@ -1,4 +1,51 @@
-{ config, pkgs, ... }:
+# NB: For now, this doesn't store zim files in the nix store. Since
+# they may be hundreds of gigabytes, we may want to store them on the
+# same disk as the rest of the nix store. TODO: deal with this and
+# keep zim files in the nix store for better reproducibility.
+
+{ config, pkgs, lib, ... }:
+let
+  kiwixLocalPort = "8002";
+  zimDirectory = "/var/archives/data/kiwix";
+  startKiwixWithDirectory = pkgs.writeScriptBin "kiwix-server-start" ''
+    #!/run/current-system/sw/bin/bash
+    zimFiles=(${zimDirectory}/*.zim);
+    ${pkgs.kiwix-tools}/bin/kiwix-serve -d -p ${kiwixLocalPort} ''${zimFiles[@]};
+  '';
+
+in
 {
-  environment.systemPackages = with pkgs; [ kiwix kiwix-tools ];
+  systemd.services.kiwix-server = {
+    wantedBy = ["multi-user.target"];
+    after = ["network.target"];
+    description = "Run a local kiwix server";
+    serviceConfig = {
+      Type = "forking";
+      User = "kiwix";
+      ExecStart = ''
+        ${startKiwixWithDirectory}/bin/kiwix-server-start
+      '';
+    };
+  };
+
+  users.users.kiwix = {
+    isSystemUser = true;
+    description = "Kiwix server";
+    group = "users";
+  };
+
+  services.nginx.virtualHosts."wiki.borg.cube" = {
+    onlySSL = true;
+    sslCertificate = "/var/www/ssl-keys/borg.cube.crt";
+    sslCertificateKey = "/var/www/ssl-keys/borg.cube.key";
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${kiwixLocalPort}";
+      proxyWebsockets = true;
+      # extraConfig = ''
+      #   add_header X-Frame-Options SAMEORIGIN always;
+      # '';
+    };
+  };
+
+  environment.systemPackages = with pkgs; [ kiwix-tools ];
 }
