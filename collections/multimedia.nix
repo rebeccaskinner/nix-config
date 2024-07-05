13f16d96ab19a67d4d572c2093d6a3f2ce5640e0
rebecca skinner <rebecca@rebeccaskinner.net>
2024-07-04 19:21:29 -0500

    remove some multimedia applications

--- collections/multimedia.nix
+++ collections/multimedia.nix
@@ -8,7 +8,7 @@ let
   whisper-cpp = pkgs.callPackage ./whisper-cpp { nvidia_x11 = pkgs.linuxPackages.nvidia_x11; gcc = pkgs.gcc11; };
 in
 utils.env.packagesEnvironment ([vlc libbluray] ++ (with pkgs;
-  [ makemkv
+  [ # makemkv
     mkvtoolnix
     handbrake
     mplayer
@@ -21,7 +21,7 @@ utils.env.packagesEnvironment ([vlc libbluray] ++ (with pkgs;
     yt-dlp
     clementine
     mat2
-    whisper-cpp
+#    whisper-cpp
     k3b
     ccextractor
   ]))
