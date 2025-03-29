46ac56cc3b57ec2531e449661be5167ae5d27f2a
rebecca skinner <rebecca@rebeccaskinner.net>
2025-03-29 17:12:08 -0500

    fix miniflux

--- nixos-configurations/daystrom/miniflux.nix
+++ nixos-configurations/daystrom/miniflux.nix
@@ -1,30 +1,21 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, config, lib, http-port ? "8181", ... }:
 {
-  environment.etc."miniflux-temp-password".text = "changeMe!0000?";
   services.miniflux = {
     enable = true;
-    adminUsername = "rebecca";
-    adminPasswordFile = "/etc/miniflux-temp-password";
     database.createLocally = true;
+    config = {
+      BASE_URL = "https://news.borg.cube/";
+      PORT = http-port;
+    };
+  };
+  services.nginx.virtualHosts."news.borg.cube" = {
+    onlySSL = true;
+    sslCertificate = "/var/www/ssl-keys/wildcard.borg.cube.crt";
+    sslCertificateKey = "/var/www/ssl-keys/wildcard.borg.cube.key";
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${http-port}";
+      proxyWebsockets = true;
+    };
   };
 
-  ## Stub for automatically adding additional users if necessary
-  # systemd.services.miniflux.serviceConfig.ExecStartPost = lib.mkAfter [
-  #   (pkgs.writeShellScript "miniflux-create-users" ''
-  #     set -euo pipefail
-  #     export DATABASE_URL="postgres://miniflux:$(<${config.services.miniflux.database.passwordFile})@localhost/miniflux?sslmode=disable"
-
-  #     create_user() {
-  #       if ! ${pkgs.miniflux}/bin/miniflux -list-users | grep -q "^$1 "; then
-  #         echo "Creating user: $1"
-  #         ${pkgs.miniflux}/bin/miniflux -create-user -username "$1" -password "$2"
-  #       else
-  #         echo "User $1 already exists"
-  #       fi
-  #     }
-
-  #     create_user alice "$(cat /run/secrets/miniflux-alice-password)"
-  #     create_user bob "$(cat /run/secrets/miniflux-bob-password)"
-  #   '')
-  # ];
 }
