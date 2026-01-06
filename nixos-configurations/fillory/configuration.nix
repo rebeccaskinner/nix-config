7cbbfacdbfd755f07927ff52712b56c5b7602470
rebecca skinner <rebecca@rebeccaskinner.net>
2026-01-05 18:23:36 -0600

    nixos-cuda.org substituter

--- nixos-configurations/fillory/configuration.nix
+++ nixos-configurations/fillory/configuration.nix
@@ -5,7 +5,11 @@
 { config, pkgs, ... }:
 {
   # nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+    substituters = [ "https://cache.nixos-cuda.org" ];
+    trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+  };
 
   imports =
     [ # Include the results of the hardware scan.
