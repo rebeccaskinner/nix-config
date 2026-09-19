53ddbf909d66c4b09dd39fdc8a5ae8d34b52160f
rebecca skinner <rebecca@rebeccaskinner.net>
2026-09-18 21:35:23 -0500

    desktop environment cleanup

--- desktop-environment/xmonad/polybar/default.nix
+++ desktop-environment/xmonad/polybar/default.nix
@@ -3,32 +3,24 @@
 # for more examples
 
 { config, pkgs, ... }:
-  let
-    polybarService = pkgs.polybar.override {
-      pulseSupport = true;
-    };
-    xdgUtils = pkgs.xdg_utils.overrideAttrs (
-      old: {
-        nativeBuildInputs = old.nativeBuildInputs or [] ++ [ pkgs.makeWrapper ];
-        postInstall = old.postInstall + "\n" + ''
-        wrapProgram $out/bin/xdg-open --suffix PATH : /run/current-system/sw/bin --suffix BROWSER : ${browser}
-        '';
-      }
-    );
-    browser = "${pkgs.firefox-beta-bin}/bin/firefox";
-    xmonad = ''
+let
+  polybarService = pkgs.polybar.override {
+    pulseSupport = true;
+  };
+  
+  xmonad = ''
       [module/xmonad]
       type = custom/script
       exec = ${pkgs.xmonad-log}/bin/xmonad-log
       tail = true
     '';
-  in
-    {
-      services.polybar = {
-        enable = true;
-        package = polybarService;
-        config = ./config.ini;
-        extraConfig = xmonad;
-        script = ''polybar top & disown'';
-      };
-    }
+in
+{
+  services.polybar = {
+    enable = true;
+    package = polybarService;
+    config = ./config.ini;
+    extraConfig = xmonad;
+    script = ''polybar top & disown'';
+  };
+}
--- profiles/desktop-environment/xmonad.nix
+++ profiles/desktop-environment/xmonad.nix
@@ -1,4 +1,6 @@
-{pkgs, primaryUser, ...}:
+# XMonad desktop: X server, sddm, and the tray applets, compositor, bar,
+# launcher, and notification daemon that make a bare window manager livable.
+{ pkgs, primaryUser, inputs, system, ... }:
 {
   services = {
     dbus = {
@@ -24,22 +26,34 @@
       candy-icons
       hicolor-icon-theme
       kdePackages.breeze-gtk
-      packmanfm
+      pcmanfm
       thunar
       tumbler
       xcursor-themes
     ];
   home-manager.users.${primaryUser} = {
     imports = [
-      ../desktop-environment/xmonad/blueman.nix
-      ../desktop-environment/xmonad/dunst.nix
-      ../desktop-environment/xmonad/feh.nix
-      ../desktop-environment/xmonad/mimeApps.nix
-      ../desktop-environment/xmonad/picom.nix
-      ../desktop-environment/xmonad/polybar/default.nix
-      ../desktop-environment/xmonad/screensaver.nix
-      ../desktop-environment/xmonad/udiskie.nix
-      ../desktop-environment/xmonad/xmonad/default.nix
+      ../../desktop-environment/xmonad/blueman.nix
+      ../../desktop-environment/xmonad/dunst.nix
+      ../../desktop-environment/xmonad/feh.nix
+      ../../desktop-environment/xmonad/mimeApps.nix
+      ../../desktop-environment/xmonad/network-manager-applet.nix
+      ../../desktop-environment/xmonad/picom.nix
+      ../../desktop-environment/xmonad/polybar/default.nix
+      ../../desktop-environment/xmonad/screensaver.nix
+      ../../desktop-environment/xmonad/udiskie.nix
+      ../../desktop-environment/xmonad/xmonad/default.nix
     ];
+
+    programs.rofi = {
+      enable = true;
+      terminal = "${pkgs.kitty}/bin/kitty";
+      theme = ../../configs/rofi/themes/darkplum.rasi;
+      plugins = with pkgs; [
+        rofi-emoji
+        rofi-calc
+        inputs.rofi-hoogle.packages.${system}.rofi-hoogle
+      ];
+    };
   };
 }
